cc7fe01270eacc33580eeefa59f55340ea088f5f
peccu <peccul@gmail.com>
2020-09-19 10:56:59 +0900

    add create feed source form

--- src/pages/AllFeeds.vue
+++ src/pages/AllFeeds.vue
@@ -2,12 +2,31 @@
   <main>
     <div class="header">
       <h1>📔 Feeds</h1>
+      <div>
+        <!-- fix to stop page from refreshing when hitting enter:
+             https://stackoverflow.com/questions/2215462/html-form-when-i-hit-enter-it-refreshes-page -->
+        <form onkeypress="return event.keyCode != 13">
+          <input
+            v-model="feedsource.title"
+            required
+            type="text"
+            placeholder="Name of new feedsource"
+            @keyup.enter="submit()"
+          />
+          <input
+            name="create feedsource"
+            value="Create Feedsource"
+            type="button"
+            @click="submit()"
+          />
+        </form>
+      </div>
     </div>
 
     <FeedArticle
-      v-for="(item, index) in allJournals"
+      v-for="(item, index) in allFeedsources"
       :key="index"
-      :journal="{ item, index }"
+      :feedsource="{ item, index }"
     />
   </main>
 </template>
